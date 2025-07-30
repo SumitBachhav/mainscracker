28f707021384d9455e9c5c4cf10312c542d9ccf4
TanseerKhan <khantanseer43@gmail.com>
2025-07-30 10:43:45 +0530

    db connection

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config;
+const sequelize = require("./config/db");
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+  }
+})();
