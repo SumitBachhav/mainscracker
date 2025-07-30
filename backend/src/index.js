489f49dbc1d78c9a00fc097d43acb5b501e61181
TanseerKhan <khantanseer43@gmail.com>
2025-07-30 10:47:32 +0530

    db sync

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const sequelize = require("./config/db");
 (async () => {
   try {
     await sequelize.authenticate();
+    // await sequelize.sync({alter: true});
+    // console.log("All models were synchronized.");
     console.log("Database connection established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database: ", error);
